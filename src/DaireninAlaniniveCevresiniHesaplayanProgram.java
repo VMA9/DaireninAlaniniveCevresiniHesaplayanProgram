272a511ee80cc591a7e8ef19d12ff3ba742bfcd2
VMA9 <111783796+VMA9@users.noreply.github.com>
2022-09-09 23:14:10 +0300

    Update DaireninAlaniniveCevresiniHesaplayanProgram.java

--- src/DaireninAlaniniveCevresiniHesaplayanProgram.java
+++ src/DaireninAlaniniveCevresiniHesaplayanProgram.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class DaireninAlaniniveCevresiniHesaplayanProgram {
+import java.util.Scanner;
+public class DaireninAlaniniveCevresiniHesaplayanProgram {
+    public static void main(String[] args) {
+        int r;
+        double pi = 3.14;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Dairenin Yarıçapını Giriniz : ");
+        r = input.nextInt();
+
+
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+
+        System.out.println("Dairenin Alanı : " + alan);
+        System.out.println("Dairenin Çevresi : " + cevre);
+
+    }
 }
