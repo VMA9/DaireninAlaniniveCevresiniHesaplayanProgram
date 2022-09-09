9518a658abf11bae9868dfe166ceb24d8dc61849
VMA9 <111783796+VMA9@users.noreply.github.com>
2022-09-09 23:44:41 +0300

    Update DaireninAlaniniveCevresiniHesaplayanProgram.java

--- src/DaireninAlaniniveCevresiniHesaplayanProgram.java
+++ src/DaireninAlaniniveCevresiniHesaplayanProgram.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 public class DaireninAlaniniveCevresiniHesaplayanProgram {
     public static void main(String[] args) {
-        int r;
-        double pi = 3.14;
+        int r , a;
+        double pi = 3.14, vAlan;
 
         Scanner input = new Scanner(System.in);
 
+
         System.out.print("Dairenin Yarıçapını Giriniz : ");
         r = input.nextInt();
+        System.out.print("Merkez Açı Ölçüsünü Giriniz : ");
+        a = input.nextInt();
+        
+        vAlan = (pi*( r * r )* a)/360;
+        System.out.println("Dairenin Diliminin Alanı : " + vAlan);
 
-
-        double alan = pi * r * r;
-        double cevre = 2 * pi * r;
-
-        System.out.println("Dairenin Alanı : " + alan);
-        System.out.println("Dairenin Çevresi : " + cevre);
 
     }
 }
